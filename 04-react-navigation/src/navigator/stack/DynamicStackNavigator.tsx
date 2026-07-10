6241f63aa3d4da8b6c10474a22048af1f9effe18
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-11 00:15:49 +0530

    feat: Enhance navigation headers with custom styles and titles via Global styles., specific Screen Style, Style via useLayoutEffect hook

--- 04-react-navigation/src/navigator/stack/DynamicStackNavigator.tsx
+++ 04-react-navigation/src/navigator/stack/DynamicStackNavigator.tsx
@@ -11,8 +11,42 @@ const Stack = createNativeStackNavigator();
 
 function MyStack(){
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Navigator 
+        screenOptions={{
+            // global options for all the screens
+            headerStyle: {
+                backgroundColor: '#0147A7',
+            },
+            headerTintColor: '#fff',
+            headerTitleAlign: 'center',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+            headerBackTitle: 'Back',
+            headerBackTitleStyle: {
+                fontSize: 16,
+            },
+        }}
+        >
+            <Stack.Screen name='Home' component={HomeScreen} 
+            options={{
+                //NOTE: these will be only for that specific screen and will override the global options
+                // headerShown: false, to hide the header
+                title: 'Overview',
+                headerStyle: {
+                    backgroundColor: '#01579B',
+                },
+                headerTintColor: '#fff',
+                headerTitleAlign: 'center',
+                // font specific to the header title in this case we are making it bold
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+                headerBackTitle: 'Back',
+                headerBackTitleStyle: {
+                    fontSize: 16,
+                },
+            }} />
             <Stack.Screen name='Details' component={DetailScreen} />
             <Stack.Screen name='Profile' component={ProfileScreen}/>
         </Stack.Navigator>
--- 04-react-navigation/src/screens/DetailScreen.tsx
+++ 04-react-navigation/src/screens/DetailScreen.tsx
@@ -1,19 +1,37 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
-const DetailScreen = ({route}: any) => {
-    const naviagtion = useNavigation<any>()
-    const {username} = route.params; 
+const DetailScreen = ({ route }: any) => {
+  const naviagtion = useNavigation<any>();
+  const { username } = route.params;
+
+  useLayoutEffect(() => {
+    naviagtion.setOptions({
+      // title: `${username} Details`,
+      title: "Ankit Details",
+      headerStyle: {
+        backgroundColor: "#01579B",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    });
+  }, [naviagtion]);
+  // this useLayoutEffect is used to set the options for the header in this case we are setting the title, headerStyle, headerTintColor, headerTitleStyle
   return (
     <View>
       <Text>{username}: DetailScreen</Text>
       {/* <Button title="Go Back" onPress={() => naviagtion.goBack()} /> */}
-      <Button title="Go Profile" onPress={() => naviagtion.navigate("Profile")} />
+      <Button
+        title="Go Profile"
+        onPress={() => naviagtion.navigate("Profile")}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default DetailScreen
+export default DetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
