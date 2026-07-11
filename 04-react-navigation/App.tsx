4b9491022abd9cf08cfdc31276663ab1aca30048
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-11 16:09:42 +0530

    feat: Implement static - bottom tab navigation with Home & Profile screens in App component

--- 04-react-navigation/App.tsx
+++ 04-react-navigation/App.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
+import { createStaticNavigation, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
 import DetailScreen from './src/screens/DetailScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
+// import ProfileScreen from './src/screens/ProfileScreen';
 import DynamicStackNavigator from './src/navigator/stack/DynamicStackNavigator';
 import StaticStackNavigator from './src/navigator/stack/StaticStackNavigator';
+
+import {
+  createBottomTabNavigator,
+  createBottomTabScreen,
+} from '@react-navigation/bottom-tabs';
+import { Button } from '@react-navigation/elements';
 
 // function HomeScreen() {
 //   return (
@@ -31,7 +37,52 @@ import StaticStackNavigator from './src/navigator/stack/StaticStackNavigator';
 
 
 
-export default function App(){
-  return <StaticStackNavigator/>
-  // return <DynamicStackNavigator/>
+// export default function App(){
+//   return <StaticStackNavigator/>
+//   // return <DynamicStackNavigator/>
+// }
+
+
+
+//!Bottom Tab Navigator
+
+function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')}>
+        Go to Profile
+      </Button>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
+    </View>
+  );
+}
+
+const MyTabs = createBottomTabNavigator({
+  screens: {
+    Home: createBottomTabScreen({
+      screen: HomeScreen,
+    }),
+    Profile: createBottomTabScreen({
+      screen: ProfileScreen,
+    }),
+  },
+});
+
+const Navigation = createStaticNavigation(MyTabs);
+
+export default function App() {
+  return <Navigation />;
 }
