718bc6ebb6d11a874042b3c35ec8293c40f0bd41
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-12 15:58:57 +0530

    chore: read about the _layout.tsx file for sibbling  and slot  used in the layout & navigating b/w screens with the help of <LInk>

--- 05-expo-router/src/app/_layout.tsx
+++ 05-expo-router/src/app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View style={{flex: 1}}>
+    <Text>Header</Text>
+      <Slot/>;
+    <Text>Footer</Text>
+    </View>
+  );
 }
--- 05-expo-router/src/app/index.tsx
+++ 05-expo-router/src/app/index.tsx
@@ -4,16 +4,17 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Link href={"/userId/123"}>UserId Page</Link>
+      <Text>Hello World</Text>
+      <Link href={'/about'}>Go to About Page</Link>
+      {/* <Link href={"/userId/123"}>UserId Page</Link> */}
       {/* here we are passing for username route but how expo router will identify it and pass it to the UserNameScreen ? */}
       {/* still moving to the userId page only so there is conflict --> fix: move to nested routes & if you still want so keep only 1 dynamic route at root level */}
-      <Link href={"/userName/John"}>UserName Page</Link> 
-      <Link href={"/docs/expo-router"}>Go to expo-router docs Page</Link> 
-      <Link href={"/docs/expo-router/installation"}>Go to expo-router/installation Page</Link> 
-      <Link href={'/Doc-02/ankit'}>Go to Ankit Page</Link>
-      <Link href={'/Doc-02/ankit/kumar'}>Go to Ankit Kumar Page</Link>
-      <Link href={'/Doc-02/ankit/kumar/bookmarks'}>Go to Ankit Kumar Bookmarks Page</Link>
+      {/* <Link href={"/userName/John"}>UserName Page</Link>  */}
+      {/* <Link href={"/docs/expo-router"}>Go to expo-router docs Page</Link>  */}
+      {/* <Link href={"/docs/expo-router/installation"}>Go to expo-router/installation Page</Link>  */}
+      {/* <Link href={'/Doc-02/ankit'}>Go to Ankit Page</Link> */}
+      {/* <Link href={'/Doc-02/ankit/kumar'}>Go to Ankit Kumar Page</Link> */}
+      {/* <Link href={'/Doc-02/ankit/kumar/bookmarks'}>Go to Ankit Kumar Bookmarks Page</Link> */}
     </View>
   );
 }
