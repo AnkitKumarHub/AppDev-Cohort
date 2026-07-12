b78135bc61851fb9c0a3c64557c6d802799678af
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-12 17:18:15 +0530

    feat: In RootLayout with stack -> designing per screen vs overall

--- 05-expo-router/src/app/_layout.tsx
+++ 05-expo-router/src/app/_layout.tsx
@@ -3,7 +3,17 @@ import { Slot, Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack/>
+  return <Stack screenOptions={{
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    headerTintColor: 'white',
+    headerTitleStyle: { fontWeight: 'bold' }
+  }}>
+    {/* <Stack.Screen name="index" options={{ title: 'Home' }} />  here you can set the design per screen in options */}
+    <Stack.Screen name="index" options={{ title: 'Home' }} /> 
+    <Stack.Screen name="about" options={{ title: 'About' }} />
+  </Stack>
   // return (
   //   <View style={{flex: 1}}>
   //   <Text>Header</Text>
