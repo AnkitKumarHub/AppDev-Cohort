cbca2b477edbd31aa0372d5b6f1302d5f2544050
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-14 20:03:19 +0530

    feat: Implemented user API with GET and POST methods, added dynamic user retrieval, and integrated database client - EXPO BACKEND API

---
+++ 07-Networking-with-Backend-Integeration/src/app/api/hello+api.ts
@@ -0,0 +1,4 @@
+// GET API endPoint
+export function GET(request: Request){
+    return Response.json({message: 'Hello, world!'})
+}
---
+++ 07-Networking-with-Backend-Integeration/src/app/api/users/[id]+api.ts
@@ -0,0 +1,44 @@
+//this is a dynamic route
+import {db} from '@/lib/db'
+
+// context type
+type Ctx = {
+    params: {id:string} 
+}
+
+
+// get a user
+export async function GET(_req:Request, {params}:Ctx){
+try {
+    const result = await db.execute({
+        sql: 'SELECT * FROM users_data WHERE id = ?',
+        args: [params.id]
+    })
+    if(result.rows.length === 0){
+        return Response.json({
+            error: 'User not found',
+            status: 404
+        })
+    }
+    return Response.json({
+        data: result.rows,
+        status: 200
+    })
+} catch (error) {
+    return Response.json({
+        error: 'Failed to get user',
+        status: 500
+    })
+}
+}
+
+// update a user
+export async function PATCH(requset: Request, {params}:Ctx){
+
+}
+
+// delete a user
+export async function DELETE(requset: Request, {params}:Ctx){
+
+}
+
