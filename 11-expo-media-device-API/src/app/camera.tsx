2534759cade902249178c5fb218233f60454e24c
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-22 19:30:44 +0530

    basic record video setup using expo-camera

--- 11-expo-media-device-API/src/app/camera.tsx
+++ 11-expo-media-device-API/src/app/camera.tsx
@@ -2,14 +2,22 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useRef, useState } from "react";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { CameraView, useCameraPermissions } from "expo-camera";
+import {
+  CameraView,
+  useCameraPermissions,
+  useMicrophonePermissions,
+} from "expo-camera";
 import { Image } from "expo-image";
 
 const Camera = () => {
   const [permission, requestPermission] = useCameraPermissions();
   const [ready, setReady] = useState(false); // Camera is ready to take photos and we are tracking because cant click photos before camera is ready
-  const cameraRef = useRef<CameraView>(null);  // for persisting the old value we use useRef 
-  const [photoUri, setPhotoUri] = useState <string | null> (null);
+  const cameraRef = useRef<CameraView>(null); // for persisting the old value we use useRef
+  const [photoUri, setPhotoUri] = useState<string | null>(null);
+
+  const [micPermission, requestMicPermission] = useMicrophonePermissions();
+  const [videoUri, setVideoUri] = useState<string | null>(null); // for storing the video uri
+  const [recording, setRecording] = useState(false); // for tracking the recording state
 
   if (!permission) return <ThemedText>No access to camera</ThemedText>;
   if (!permission.granted) {
@@ -26,10 +34,26 @@ const Camera = () => {
     );
   }
 
-  const takePhoto = async()=>{
-    const photo = await cameraRef.current?.takePictureAsync({quality: 1})
-    if(photo?.uri) setPhotoUri(photo.uri); // set the photo uri to the state
-  }
+  const takePhoto = async () => {
+    const photo = await cameraRef.current?.takePictureAsync({ quality: 1 });
+    if (photo?.uri) setPhotoUri(photo.uri); // set the photo uri to the state
+  };
+
+  const startRecording = async () => {
+    if (!micPermission?.granted) {
+      const result = await requestMicPermission();
+      if (!result?.granted) return;
+    }
+
+    setRecording(true);
+    const video = await cameraRef.current?.recordAsync({ maxDuration: 15 });
+    setVideoUri(video?.uri ?? null);
+    setRecording(false);
+  };
+
+  const stopRecording = async () => {
+    cameraRef.current?.stopRecording();
+  };
 
   return (
     <ThemedView style={{ flex: 1 }}>
@@ -37,17 +61,26 @@ const Camera = () => {
         ref={cameraRef}
         style={{ flex: 1 }}
         facing="back"
+        mode="video"
         onCameraReady={() => setReady(true)}
         onMountError={({ message }) => console.warn(message)}
       />
 
-      <Button title="Take Photo" onPress={takePhoto} disabled={!ready}/>
+      {/* <Button title="Take Photo" onPress={takePhoto} disabled={!ready}/>
       {photoUri && (
         <Image source={{uri: photoUri}} 
         style={{height: 200 }}
         contentFit = "cover"
         />
-      )}
+      )} */}
+
+      <Button
+        title={recording ? "Stop Recording" : "Start Recording"}
+        disabled={!ready}
+        onPress={recording ? stopRecording : startRecording}
+      />
+
+      {videoUri && <ThemedText selectable>{videoUri}</ThemedText>}
 
       <ThemedText style={{ padding: 12 }}>
         {ready ? "camera ready" : "starting camera..."}
