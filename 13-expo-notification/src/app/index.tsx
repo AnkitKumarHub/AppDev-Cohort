c6c3e373e8f2903ede8691c3f396301ccaf34d78
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-25 20:27:54 +0530

    feat: Enhance notification handling with custom sound, data payload, and response listener

--- 13-expo-notification/src/app/index.tsx
+++ 13-expo-notification/src/app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet, Button } from "react-native";
 import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -15,14 +16,33 @@ export default function Index() {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Welcome!",
+        subtitle: "From ankit",
         body: "This is my first notification!",
+        data: {
+          screen: "/profile",
+          userId: 42,
+        },
+        sound: false, // false means no sound, defaultRingtone & defaultCritical are IOS specific for now
+        badge: 3, // only works on ios
+        priority: Notifications.AndroidNotificationPriority.HIGH,
+        sticky: true
       },
       trigger: null,
     });
   }
+
+  useEffect(() => {
+    Notifications.addNotificationResponseReceivedListener((response) => {
+      const data = response.notification.request.content.data;
+      console.log(data);
+      //  when user clicks on the notification, we can navigate to the screen specified in the data
+      //  const url = response.notification.request.content.data.url;
+      // Linking.openURL(url);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Button title="Schedule Notification" onPress={example1}/>
+      <Button title="Schedule Notification" onPress={example1} />
     </View>
   );
 }
