2a5fd79eccc91f090e145f447949e25884c160e3
AnkitKumarHub <akkimessi710@gmail.com>
2026-07-28 22:16:59 +0530

    feat: Authentication with betterAuth

---
+++ 14-expo-authentication/src/lib/auth.ts
@@ -0,0 +1,40 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+import { expo } from "@better-auth/expo";
+
+export const auth = betterAuth({
+    database: new Pool({
+        connectionString: process.env.DATABASE_URL
+    }),
+    plugins: [expo()],
+
+    emailAndPassword: { 
+        enabled: true, 
+      }, 
+      account:{
+        accountLinking: {
+            enabled: true,
+            trustedProviders: ["github", "google", "email-password"],
+        }
+      },
+      socialProviders: { 
+        github: { 
+          clientId: process.env.GITHUB_CLIENT_ID as string, 
+          clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+        google: { 
+          clientId: process.env.GOOGLE_CLIENT_ID as string, 
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+      }, 
+
+      rustedOrigins: [
+        "14expoauthentication://",
+        
+        ...(process.env.NODE_ENV === "development" ? [
+            "exp://",                      // Trust all Expo URLs (prefix matching)
+            "exp://**",                    // Trust all Expo URLs (wildcard matching)
+            "exp://192.168.*.*:*/**",      // Trust 192.168.x.x IP range with any port and path
+        ] : [])
+    ]
+})
